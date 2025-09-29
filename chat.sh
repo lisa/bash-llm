f527f3042ad738f10ccb4b5f2c5c59434a15f387
Lisa Seelye <18159+lisa@users.noreply.github.com>
2025-09-28 20:53:03 -0400

    Create chat.sh

---
+++ chat.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# This is the large language model for Bash. It will mimic the best large language models the tech industry has to offer. Requires Linux or MacOS due to special UNIX files
+cat /dev/urandom
